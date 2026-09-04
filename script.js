b0b9c5d0d8ccddc990681fcd250c5cc4725bb668
Kartikeya Mishra <Kartikeya9127@outlook.com>
2026-09-04 19:01:14 +0530

    the multiple funtions work now and the code is much cleaner. adding multuple electrons possible

--- script.js
+++ script.js
@@ -2,6 +2,97 @@ const canvas = document.getElementById("background");
 
 const ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(200, 200, 50, 0, Math.PI)
-ctx.fill();
+
+
+
+canvas.width = window.innerWidth;
+
+canvas.height = window.innerHeight;
+
+const centerX = canvas.width / 2;
+
+const centerY = canvas.height / 2;
+
+let rotation = 0;
+
+
+
+
+
+
+function drawNucleus(){
+
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, 30, 0, Math.PI*2);
+    ctx.fill();
+
+}
+
+
+
+
+function drawShell() {
+
+    ctx.beginPath();
+
+    ctx.arc(centerX, centerY, 180, 0, Math.PI * 2);
+    ctx.stroke();
+
+}
+
+
+
+function drawElectron(){
+    const electronCount= 4;
+
+    const angleStep = (Math.PI * 2) / electronCount;
+    const radius = 180;
+
+
+    for (let i = 0; i < 4; i++){
+
+
+        const angle = i * angleStep + rotation;
+
+        const electronX = centerX + radius*Math.cos(angle);
+        const electronY = centerY + radius *Math.sin(angle);
+
+        ctx.beginPath();
+
+        ctx.arc(electronX, electronY, 8, 0, Math.PI * 2);
+        ctx.fill();
+
+
+    }
+  
+
+
+}
+
+function electronMovement(){
+
+    
+    rotation = rotation + 0.02;
+     
+    ctx.clearRect(0, 0, canvas.width, canvas.height )
+
+    drawNucleus();
+
+    drawElectron();
+
+    drawShell();
+    requestAnimationFrame(electronMovement);
+}     
+
+
+
+electronMovement();
+
+
+
+
+
+
+
+
+
